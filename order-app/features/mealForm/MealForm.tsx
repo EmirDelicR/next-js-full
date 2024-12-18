8889c599f44d8bfa6cd01b116c5222d6b7f81b3f
EmirDelicR <emirdelictbf@gmail.com>
2024-12-18 15:31:15 +0100

    bugfix: add validation

--- order-app/features/mealForm/MealForm.tsx
+++ order-app/features/mealForm/MealForm.tsx
@@ -1,18 +1,32 @@
+'use client';
+
+import { useActionState } from 'react';
 import Form from 'next/form';
-import { Button, Stack, Textarea, TextInput } from '@mantine/core';
+import { Alert, Button, Stack, Textarea, TextInput } from '@mantine/core';
 import { shareMeal } from '@/lib/actions/actions';
 
 export default function MealForm() {
+  const [state, formAction, isPending] = useActionState(shareMeal, null);
+
   return (
-    <Form action={shareMeal} default>
+    <Form action={formAction}>
       <Stack gap="md">
-        <TextInput name="title" withAsterisk label="Title" placeholder="Meal title" />
+        <TextInput
+          name="title"
+          withAsterisk
+          label="Title"
+          placeholder="Meal title"
+          defaultValue={state?.meal?.title || ''}
+          error={state?.messages?.title}
+        />
         <TextInput
           withAsterisk
           label="Slug"
           placeholder="Slug"
           name="slug"
           description="Add specific slug to meal for easy recognition."
+          defaultValue={state?.meal?.slug || ''}
+          error={state?.messages?.slug}
         />
         <Textarea
           withAsterisk
@@ -20,8 +34,13 @@ export default function MealForm() {
           name="summary"
           description="Add summary about this meal"
           placeholder="Summary"
+          defaultValue={state?.meal?.summary || ''}
+          error={state?.messages?.summary}
         />
-        <Button type="submit">Add meal</Button>
+        <Button type="submit" disabled={isPending}>
+          {isPending ? 'Submitting...' : 'Add meal'}
+        </Button>
+        {state?.isError && <Alert color="red">{state.message}</Alert>}
       </Stack>
     </Form>
   );
--- order-app/lib/actions/actions.ts
+++ order-app/lib/actions/actions.ts
@@ -4,14 +4,49 @@ import { redirect } from 'next/navigation';
 import { saveMeal } from '../db/db';
 import { Meal } from '../types/app.type';
 
-export async function shareMeal(formData: FormData) {
+const validateField = (text: string, field: 'Title' | 'Summary' | 'Slug') => {
+  return !text || text.trim() === '' ? `${field} must be set!` : null;
+};
+
+export async function shareMeal(_previousState: unknown, formData: FormData) {
+  let redirectPath: string | null = null;
+  const title = formData.get('title') as string;
+  const summary = formData.get('summary') as string;
+  const slug = formData.get('slug') as string;
+
+  const validationError = {
+    title: validateField(title, 'Title'),
+    summary: validateField(summary, 'Summary'),
+    slug: validateField(slug, 'Slug'),
+  };
   const meal: Meal = {
-    title: formData.get('title') as string,
-    summary: formData.get('summary') as string,
-    slug: formData.get('slug') as string,
+    title,
+    summary,
+    slug,
   };
 
-  await saveMeal(meal);
+  if (validationError.title || validationError.summary || validationError.slug) {
+    return {
+      message: null,
+      messages: { ...validationError },
+      isError: false,
+      meal,
+    };
+  }
 
-  redirect('/meals');
+  try {
+    await saveMeal(meal);
+    redirectPath = '/meals';
+  } catch (err) {
+    return {
+      message: `Error occurred: ${err}`,
+      messages: { ...validationError },
+      isError: true,
+      meal,
+    };
+  } finally {
+    if (redirectPath) {
+      redirect(redirectPath);
+    }
+  }
 }
